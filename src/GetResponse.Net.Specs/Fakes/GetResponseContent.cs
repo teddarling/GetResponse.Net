7540838c3e2e67a0d16c06e511875fd30486b6a0
Ted Darling <ted@skybytetech.com>
2013-08-16 16:17:37 -0500

    Setup fake HttpContent for spec tests

---
+++ src/GetResponse.Net.Specs/Fakes/GetResponseContent.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetResponse.Net.Specs.Fakes
+{
+    public class GetResponseContent : HttpContent
+    {
+        private string _content;
+
+        public GetResponseContent(string content)
+        {
+            _content = content;
+        }
+
+        protected async override Task SerializeToStreamAsync(
+            Stream stream, 
+            TransportContext context)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(_content);
+            await stream.WriteAsync(bytes, 0, bytes.Length);
+        }
+
+        protected override bool TryComputeLength(
+            out long length)
+        {
+            length = _content.Length;
+            return true;
+        }
+    }
+}
---
+++ src/GetResponse.Net.Specs/Fakes/GetResponseMessageHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetResponse.Net.Specs.Fakes
+{
+    public class GetResponseMessageHandler
+    {
+    }
+}
