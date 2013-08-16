72d1e90494d739350e5c93a853b7a981267a0caf
Ted Darling <ted@skybytetech.com>
2013-08-16 16:08:27 -0500

    Restarted Solution and setup first feature file

---
+++ src/GetResponse.Net.Specs/Ping.feature
@@ -0,0 +1,10 @@
+﻿Feature: Ping
+	In order to test the API connection
+	As an API developer
+	I want to ping the API
+
+@mytag
+Scenario: Add two numbers
+	Given I have a valid API key 'valid key'
+	When I send a 'ping' request
+	Then the result should be 'pong'
---
+++ src/GetResponse.Net.Specs/PingSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GetResponse.Net.Specs
+{
+    [Binding]
+    public class PingSteps
+    {
+        [Given(@"I have a valid API key '(.*)'")]
+        public void GivenIHaveAValidAPIKey(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I send a '(.*)' request")]
+        public void WhenISendARequest(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be '(.*)'")]
+        public void ThenTheResultShouldBe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
