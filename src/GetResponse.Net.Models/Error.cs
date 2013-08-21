3a206b96c49f0b37a275754c648742e821d218b0
Ted Darling <ted@skybytetech.com>
2013-08-21 17:21:46 -0500

    Add error and Ping Response which should have been added previously.

---
+++ src/GetResponse.Net.Models/Error.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GetResponse.Net.Models
+{
+    public class Error
+    {
+        public string Message { get; set; }
+        public int Code { get; set; }
+    }
+}
---
+++ src/GetResponse.Net.Models/PingResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetResponse.Net.Models
+{
+    public class PingResponse : BaseResponse
+    {
+        public PingResult Result { get; set; }
+    }
+}
